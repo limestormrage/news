037fbc85c39aa293e2453a925d316c33e481dd01
Dmitry Tikhonov <dimarik-tixonov@yandex.ru>
2023-11-21 10:10:45 +0300

    router configuration

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,6 @@
 import "./styles/index.scss";
 import { Link } from "react-router-dom";
-import { Route, Routes } from "react-router-dom";
-import { Suspense } from "react";
 import { useTheme } from "app/providers/ThemeProvider";
-import { AboutPage } from "pages/AboutPage";
-import { MainPage } from "pages/MainPage";
 
 const App = (): JSX.Element => {
   const { theme, toggleTheme } = useTheme();
@@ -14,12 +10,6 @@ const App = (): JSX.Element => {
       <Link to={"/"}>Главная</Link>
       <Link to={"/about"}>О сайте</Link>
       <button onClick={toggleTheme}>TOGGLE</button>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path={"/"} element={<MainPage />} />
-          <Route path={"/about"} element={<AboutPage />} />
-        </Routes>
-      </Suspense>
     </div>
   );
 };
