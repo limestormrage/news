b47f234d08205731d9be760000b59dbbb5406a97
Dmitry Tikhonov <dimarik-tixonov@yandex.ru>
2024-05-03 16:30:53 +0300

    add modal component

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,14 +1,34 @@
 import styles from "./Navbar.module.scss";
 import { classNames } from "shared/lib/classNames/classNames";
+import { Modal } from "shared/ui/Modal/Modal";
+import { useTranslation } from "react-i18next";
+import { useCallback, useState } from "react";
+import { Button, ButtonTheme } from "shared/ui/Button/Button";
 
 interface INavbarProps {
   className?: string;
 }
 
 export const Navbar = ({ className }: INavbarProps) => {
+  const [isAuthModal, setIsAuthModal] = useState(false);
+
+  const { t } = useTranslation();
+
+  const onToggleAuthModal = useCallback(() => {
+    setIsAuthModal((prev) => !prev);
+  }, []);
+
   return (
     <div className={classNames(styles.navbar, {}, [className])}>
-      <div className={styles.links}>/</div>
+      <div className={styles.links}>
+        <Button theme={ButtonTheme.CLEAR_INVERTED} onClick={onToggleAuthModal}>
+          {t("Войти")}
+        </Button>
+      </div>
+      <Modal isOpen={isAuthModal} onClose={onToggleAuthModal}>
+        {/* eslint-disable-next-line i18next/no-literal-string */}
+        <p>тест</p>
+      </Modal>
     </div>
   );
 };
