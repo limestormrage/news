76b9a8395da5c6e56087d3c06915d1bbbdda2fa5
Dmitry Tikhonov <dimarik-tixonov@yandex.ru>
2024-07-02 19:38:09 +0300

    add reducerManager

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -7,25 +7,26 @@ import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer";
 export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] {
   const plugins = [
     new HtmlWebpackPlugin({
-      template: paths.html,
+      template: paths.html
     }),
     new webpack.ProgressPlugin(),
     new MiniCssExtractPlugin({
       filename: "css/[name].[contenthash:8].css",
-      chunkFilename: "css/[name].[contenthash:8].css",
+      chunkFilename: "css/[name].[contenthash:8].css"
     }),
     new webpack.DefinePlugin({
-      __IS_DEV__: JSON.stringify(isDev),
-    }),
+      __IS_DEV__: JSON.stringify(isDev)
+    })
   ];
+
+  plugins.push(
+    new BundleAnalyzerPlugin({
+      openAnalyzer: false
+    })
+  );
 
   if (isDev) {
     plugins.push(new webpack.HotModuleReplacementPlugin());
-    plugins.push(
-      new BundleAnalyzerPlugin({
-        openAnalyzer: false,
-      })
-    );
   }
 
   return plugins;
