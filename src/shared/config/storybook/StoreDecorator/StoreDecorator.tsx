452535952f3e3d400fa4dfa975b8a58ed32156a8
Dmitry Tikhonov <dimarik-tixonov@yandex.ru>
2024-07-02 20:27:31 +0300

    подгрузка асинхронных редюсеров для сторис

--- src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
+++ src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
@@ -1,12 +1,24 @@
-import { DeepPartial } from "@reduxjs/toolkit";
+/* eslint-disable indent */
+import { DeepPartial, ReducersMapObject } from "@reduxjs/toolkit";
 import { StoryFn } from "@storybook/react";
 import { StateSchema, StoreProvider } from "app/providers/StoreProvider";
+import { loginReducer } from "features/AuthByUsername/model/slice/loginSlice";
+
+const defaultAsyncReducer: DeepPartial<ReducersMapObject<StateSchema>> = {
+  loginForm: loginReducer
+};
 
 // eslint-disable-next-line react/display-name
-export const StoreDecorator = (state: DeepPartial<StateSchema>) => (StoryComponent: StoryFn) => {
-  return (
-    <StoreProvider initialState={state}>
-      <StoryComponent />
-    </StoreProvider>
-  );
-};
+export const StoreDecorator =
+  (state: DeepPartial<StateSchema>, asyncReducers?: DeepPartial<ReducersMapObject<StateSchema>>) =>
+  // eslint-disable-next-line react/display-name
+  (StoryComponent: StoryFn) => {
+    return (
+      <StoreProvider
+        initialState={state}
+        asyncReducers={{ ...defaultAsyncReducer, ...asyncReducers }}
+      >
+        <StoryComponent />
+      </StoreProvider>
+    );
+  };
