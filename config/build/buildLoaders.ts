5f824f97944c5151b02500d9a454b1ac48ec6d5a
Dmitry Tikhonov <limestormrage@yandex.ru>
2024-09-15 17:10:25 +0300

    add react refresh plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -2,36 +2,28 @@ import webpack from "webpack";
 import { BuildOptions } from "./types/config";
 import { buildCssLoader } from "./loaders/buildCssLoader";
 import { buildSvgLoader } from "./loaders/buildSvgLoader";
+import { buildBabelLoader } from "./loaders/buildBabelLoader";
 
 export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
   const svgLoader = buildSvgLoader();
 
   const cssLoader = buildCssLoader(isDev);
 
-  const babelLoader = {
-    test: /\.(js|jsx|tsx)$/,
-    exclude: /node_modules/,
-    use: {
-      loader: "babel-loader",
-      options: {
-        presets: ["@babel/preset-env"],
-      },
-    },
-  };
+  const babelLoader = buildBabelLoader(isDev);
 
   const typescriptLoader = {
     test: /\.tsx?$/,
     use: "ts-loader",
-    exclude: /node_modules/,
+    exclude: /node_modules/
   };
 
   const fileLoader = {
     test: /\.(png|jpe?g|gif)$/i,
     use: [
       {
-        loader: "file-loader",
-      },
-    ],
+        loader: "file-loader"
+      }
+    ]
   };
 
   return [fileLoader, svgLoader, babelLoader, typescriptLoader, cssLoader];
